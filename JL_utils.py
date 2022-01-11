eef4cd0fc50f328f36e53e94acbd7e9b566331b0
Woo Joon Lee <woojoonlee98@gmail.com>
2022-01-11 10:26:35 +0900

    naver api 활용 geocoding 함수 추가

--- JL_utils.py
+++ JL_utils.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import requests
 from haversine import haversine
-from config import kakao_api
+from config import kakao_api, naver_client_id, naver_client_secert
+from urllib import parse
 
 school_data = pd.read_csv(
     ".\전처리완료 파일\school_data_loc.csv", encoding="cp949", index_col=0)
@@ -34,6 +35,30 @@ def get_location_v2(df):
         except:
             print(f"error in {i}th row")
     return df
+
+
+def get_location_naver(address):
+    url = 'https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=' + parse.quote(
+        address)
+    header = {
+        'X-NCP-APIGW-API-KEY-ID': naver_client_id,
+        'X-NCP-APIGW-API-KEY': naver_client_secert
+    }
+    r = requests.get(url, headers=header)
+
+    if r.status_code == 200:  # 정상 리턴만 반응
+        if r.json()['addresses'][0] == []:
+            latitude = None
+            longitude = None
+        else:
+            latitude = r.json()['addresses'][0]['y']
+            longitude = r.json()['addresses'][0]['x']
+
+        result = (float(latitude), float(longitude))
+    else:
+        result = "ERROR"
+    return result
+
 
 
 #하버사인 공식을 이용하여 WGS84(위도 경도)를 바탕으로 거리를 구하는 함, 단위는 m
