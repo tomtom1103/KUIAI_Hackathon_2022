23fa67bff56eb765a00f4d1532ef344f05e2ffe2
tomtom1103 <jonghyunlee1103@gmail.com>
2022-01-13 02:09:37 +0900

    infopage

--- JL_info.py
+++ JL_info.py
@@ -7,7 +7,6 @@ def info():
     st.subheader("About the Creators🚀")
     df = pd.read_pickle("data_upload/jl.pickle")
 
-
     st.markdown('##### Journey Lee?')
 
     st.markdown('*전의 이씨는 옛 전의군 지역(소정면, 전의면, 전동면)을 본관으로 하는 대한민국의 한국의 성씨. 시조는 고려 초 개국 공신인 이도(李棹)인데, 초명은 치(齒)이며, 시호는 성절(聖節) 이다.*')
